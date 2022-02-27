e79cb6958b6f3c63083aef38b9ab5b81407ca056
Philipp Dudarenko <iphilimosha@gmail.com>
2022-02-27 14:10:38 +0300

    feat: add output up to 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    const upToTwenty = ['zero', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ',
+    'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ',
+    'seventeen ', 'eighteen ', 'nineteen '];
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    rez = '';
+
+    if(number < 20) {
+        rez =  upToTwenty[number];
+        return rez;
+    }
+
 }
